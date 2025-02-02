7c8ba470d0a7cae85d10105a07acd73c62af338b
Kim Liu <105848598+Cyneburg@users.noreply.github.com>
2025-02-02 16:11:03 +0800

    Update utils_test.py

--- utils_test.py
+++ utils_test.py
@@ -34,7 +34,6 @@ class MPPDataset(InMemoryDataset):
         return [self.dataset + '_data_mol.pt', self.dataset + '_data_clique.pt', self.dataset + '_data_pre.pt']
 
     def download(self):
-        # Download to `self.raw_dir`.
         pass
 
     def _download(self):
@@ -49,15 +48,11 @@ class MPPDataset(InMemoryDataset):
         data_list_clique = []
         data_list_pre = []
         data_len = len(xd)
-        # print('loading tensors ...')
         for i in range(data_len):
-            # print('Converting to graph: {}/{}'.format(i + 1, data_len))
             smiles = xd[i]
             labels = y[i]
             mol_size, mol_features, mol_edge_index, edge_features_list = smile_graph[smiles]
             clique_size, clique_features, clique_edge_index = clique_graph[smiles]
-            # print(labels)
-            # pre_mol =  mol_to_graph_data_obj_simple(mol)
             GCNData_mol = Data(x=torch.Tensor(mol_features),
                                edge_index=torch.LongTensor(mol_edge_index).transpose(-1, 0),
                                edge_attr= torch.LongTensor(edge_features_list),
@@ -71,34 +66,28 @@ class MPPDataset(InMemoryDataset):
 
             data_list_mol.append(GCNData_mol)
             data_list_clique.append(GCNData_clique)
-            # data_list_pre.append(pre_mol)
 
         if self.pre_filter is not None:
             data_list_mol = [data for data in data_list_mol if self.pre_filter(data)]
             data_list_clique = [data for data in data_list_clique if self.pre_filter(data)]
-            # data_list_pre = [data for data in data_list_pre if self.pre_filter(data)]
         if self.pre_transform is not None:
             data_list_mol = [self.pre_transform(data) for data in data_list_mol]
             data_list_clique = [self.pre_transform(data) for data in data_list_clique]
-            # data_list_pre = [self.pre_transform(data) for data in data_list_pre]
 
         self.data_mol = data_list_mol
         self.data_clique = data_list_clique
-        # self.data_pre = data_list_pre
 
     def __len__(self):
         return len(self.y)
 
     def __getitem__(self, idx):
-        # return GNNData_mol, GNNData_pro
-        return self.data_mol[idx], self.data_clique[idx] #, self.data_pre[idx]
+        return self.data_mol[idx], self.data_clique[idx]
 
 def one_hot_encoding(x, allowable_set):
     if x not in allowable_set:
         raise Exception('input {0} not in allowable set{1}:'.format(x, allowable_set))
     return list(map(lambda s: x == s, allowable_set))
 
-# one ont encoding with unknown symbol
 def one_hot_encoding_unk(x, allowable_set):
     if x not in allowable_set:
         x = allowable_set[-1]
@@ -130,7 +119,6 @@ def one_of_k_encoding_unk(x, allowable_set):
 
 
 def atom_features(atom):
-    # 44 +11 +11 +11 +1
     return np.array(one_hot_encoding_unk(atom.GetSymbol(),
                                          ['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', 'Na', 'Ca', 'Fe', 'As',
                                           'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb', 'Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se',
@@ -141,7 +129,6 @@ def atom_features(atom):
                     one_hot_encoding(atom.GetImplicitValence(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) +
                     [atom.GetIsAromatic()])
 
-# mol smile to mol graph edge index
 def smile_to_graph(smile):
     mol = Chem.MolFromSmiles(smile)
     mol_size = mol.GetNumAtoms()
@@ -167,25 +154,25 @@ def smile_to_graph(smile):
     return mol_size, mol_features, edge_index, edge_features_list
 
 def clique_features(clique, edges, clique_idx, smile):
-    NumAtoms = len(clique)  # 子结构中原子数
-    NumEdges = 0  # 与子结构所连的边数，子结构的度
+    NumAtoms = len(clique) 
+    NumEdges = 0  
     for edge in edges:
         if clique_idx == edge[0] or clique_idx == edge[1]:
             NumEdges += 1
     mol = Chem.MolFromSmiles(smile)
     atoms = []
-    NumHs = 0  # 基团中氢原子的个数
+    NumHs = 0 
     NumImplicitValence = 0
     for idx in clique:
         atom = mol.GetAtomWithIdx(idx)
         atoms.append(atom.GetSymbol())
         NumHs += atom.GetTotalNumHs()
         NumImplicitValence += atom.GetImplicitValence()
-    # 基团中是否包含环
+
     IsRing = 0
     if len(clique) > 2:
         IsRing = 1
-    # 基团中是否有键
+
     IsBond = 0
     if len(clique) == 2:
         IsBond = 1
@@ -200,18 +187,16 @@ def clique_features(clique, edges, clique_idx, smile):
                     [IsRing]+[IsBond])
 
 def cluster_graph(mol):
-    n_atoms = mol.GetNumAtoms()  #获得分子图中原子数
-    cliques = []  # 所有的的边（化学键和一对原子组成）和环
-    for bond in mol.GetBonds():  #获得分子图中所有化学键
-        a1 = bond.GetBeginAtom().GetIdx() #获得开始原子索引
-        a2 = bond.GetEndAtom().GetIdx() #获得结束原子索引
-        if not bond.IsInRing():   #判断化学键是否在环中
-            cliques.append([a1, a2])  #将边加入到集合中
+    n_atoms = mol.GetNumAtoms()  
+    cliques = [] 
+    for bond in mol.GetBonds():  
+        a1 = bond.GetBeginAtom().GetIdx() 
+        a2 = bond.GetEndAtom().GetIdx() 
+        if not bond.IsInRing(): 
+            cliques.append([a1, a2]) 
 
-    ssr = [list(x) for x in Chem.GetSymmSSSR(mol)]  # 获得分子图中的所有环
-    cliques.extend(ssr)  #
-    # cliques 所有的的边（化学键和一对原子组成）和环
-    # nei_list为原子属于哪个基团\子结构
+    ssr = [list(x) for x in Chem.GetSymmSSSR(mol)]
+    cliques.extend(ssr)  
     nei_list = [[] for i in range(n_atoms)]
     for i in range(len(cliques)):
         for atom in cliques[i]:
@@ -229,51 +214,27 @@ def clique_to_graph(smile):
     mol = Chem.MolFromSmiles(smile)
     clique, edge = cluster_graph(mol)
 
-    c_features = []  # 特征矩阵
+    c_features = []
     for idx in range(len(clique)):
         cq_features = clique_features(clique[idx], edge, idx, smile)
         c_features.append(cq_features / sum(cq_features))
 
-    clique_size = len(clique)  # 子结构图节点数
+    clique_size = len(clique)
     return clique_size, c_features, edge
 
 def scaffold_split(dataset, smiles_list, task_idx=None, null_value=0,
                    frac_train=0.8, frac_valid=0.1, frac_test=0.1,
                    return_smiles=False):
-    """
-    Adapted from https://github.com/deepchem/deepchem/blob/master/deepchem/splits/splitters.py
-    Split dataset by Bemis-Murcko scaffolds
-    This function can also ignore examples containing null values for a
-    selected task when splitting. Deterministic split
-    :param dataset: pytorch geometric dataset obj
-    :param smiles_list: list of smiles corresponding to the dataset obj
-    :param task_idx: column idx of the data.y tensor. Will filter out
-    examples with null value in specified task column of the data.y tensor
-    prior to splitting. If None, then no filtering
-    :param null_value: float that specifies null value in data.y to filter if
-    task_idx is provided
-    :param frac_train:
-    :param frac_valid:
-    :param frac_test:
-    :param return_smiles:
-    :return: train, valid, test slices of the input dataset obj. If
-    return_smiles = True, also returns ([train_smiles_list],
-    [valid_smiles_list], [test_smiles_list])
-    """
     np.testing.assert_almost_equal(frac_train + frac_valid + frac_test, 1.0)
 
     if task_idx != None:
-        # filter based on null values in task_idx
-        # get task array
         y_task = np.array([data[0].y[task_idx].item() for data in dataset])
-        # boolean array that correspond to non null values
         non_null = y_task != null_value
         smiles_list = list(compress(enumerate(smiles_list), non_null))
     else:
         non_null = np.ones(len(dataset)) == 1
         smiles_list = list(compress(enumerate(smiles_list), non_null))
 
-    # create dict of the form {scaffold_i: [idx1, idx....]}
     all_scaffolds = {}
     for i, smiles in smiles_list:
         scaffold = generate_scaffold(smiles, include_chirality=True)
@@ -282,14 +243,12 @@ def scaffold_split(dataset, smiles_list, task_idx=None, null_value=0,
         else:
             all_scaffolds[scaffold].append(i)
 
-    # sort from largest to smallest sets
     all_scaffolds = {key: sorted(value) for key, value in all_scaffolds.items()}
     all_scaffold_sets = [
         scaffold_set for (scaffold, scaffold_set) in sorted(
             all_scaffolds.items(), key=lambda x: (len(x[1]), x[1][0]), reverse=True)
     ]
 
-    # get train, valid test indices
     train_cutoff = frac_train * len(smiles_list)
     valid_cutoff = (frac_train + frac_valid) * len(smiles_list)
     train_idx, valid_idx, test_idx = [], [], []
@@ -305,7 +264,6 @@ def scaffold_split(dataset, smiles_list, task_idx=None, null_value=0,
     assert len(set(train_idx).intersection(set(valid_idx))) == 0
     assert len(set(test_idx).intersection(set(valid_idx))) == 0
 
-    # print(dataset,train_idx)
     train_dataset = [dataset[id] for id in train_idx]
     valid_dataset = [dataset[id] for id in valid_idx]
     test_dataset = [dataset[id] for id in test_idx]
@@ -313,32 +271,10 @@ def scaffold_split(dataset, smiles_list, task_idx=None, null_value=0,
 
 def random_scaffold_split(dataset, smiles_list, task_idx=None, null_value=0,
                    frac_train=0.8, frac_valid=0.1, frac_test=0.1, seed=0):
-    """
-    Adapted from https://github.com/pfnet-research/chainer-chemistry/blob/master/chainer_chemistry/dataset/splitters/scaffold_splitter.py
-    Split dataset by Bemis-Murcko scaffolds
-    This function can also ignore examples containing null values for a
-    selected task when splitting. Deterministic split
-    :param dataset: pytorch geometric dataset obj
-    :param smiles_list: list of smiles corresponding to the dataset obj
-    :param task_idx: column idx of the data.y tensor. Will filter out
-    examples with null value in specified task column of the data.y tensor
-    prior to splitting. If None, then no filtering
-    :param null_value: float that specifies null value in data.y to filter if
-    task_idx is provided
-    :param frac_train:
-    :param frac_valid:
-    :param frac_test:
-    :param seed;
-    :return: train, valid, test slices of the input dataset obj
-    """
-
     np.testing.assert_almost_equal(frac_train + frac_valid + frac_test, 1.0)
 
     if task_idx != None:
-        # filter based on null values in task_idx
-        # get task array
         y_task = np.array([data.y[task_idx].item() for data in dataset])
-        # boolean array that correspond to non null values
         non_null = y_task != null_value
         smiles_list = list(compress(enumerate(smiles_list), non_null))
     else:
@@ -378,30 +314,13 @@ def random_scaffold_split(dataset, smiles_list, task_idx=None, null_value=0,
 def random_split(dataset, task_idx=None, null_value=0,
                    frac_train=0.8, frac_valid=0.1, frac_test=0.1, seed=0,
                  smiles_list=None):
-    """
-
-    :param dataset:
-    :param task_idx:
-    :param null_value:
-    :param frac_train:
-    :param frac_valid:
-    :param frac_test:
-    :param seed:
-    :param smiles_list: list of smiles corresponding to the dataset obj, or None
-    :return: train, valid, test slices of the input dataset obj. If
-    smiles_list != None, also returns ([train_smiles_list],
-    [valid_smiles_list], [test_smiles_list])
-    """
     np.testing.assert_almost_equal(frac_train + frac_valid + frac_test, 1.0)
 
     if task_idx != None:
-        # filter based on null values in task_idx
-        # get task array
         y_task = np.array([data.y[task_idx].item() for data in dataset])
-        non_null = y_task != null_value  # boolean array that correspond to non null values
+        non_null = y_task != null_value
         idx_array = np.where(non_null)[0]
-        dataset = dataset[torch.tensor(idx_array)]  # examples containing non
-        # null labels in the specified task_idx
+        dataset = dataset[torch.tensor(idx_array)]
     else:
         pass
 
@@ -441,7 +360,6 @@ def creat_data(datasets, mode, seed):
                 clique = clique_to_graph(smile)
                 smile_graph[smile] = g
                 clique_graph[smile] = clique
-                # convert to PyTorch data format
             random.seed(seed)
             df = pd.read_csv('data/baidu/train.csv')
             random_num = random.sample(range(0, len(df)), len(df))
@@ -452,7 +370,6 @@ def creat_data(datasets, mode, seed):
             data_valid = df.loc[valid_num]
             drug_train, label_train, drug_valid, label_valid = list(data_train['smiles']), list(data_train['label']),list(data_valid['smiles']), list(data_valid['label'])
             drug_train, label_train, drug_valid, label_valid = np.asarray(drug_train), np.asarray(label_train),np.asarray(drug_valid), np.asarray(label_valid)
-            # make data PyTorch Geometric ready
             print('开始创建数据')
             print(datasets)
             train_data = MPPDataset(root='data', dataset=datasets, xd=drug_train, y=label_train, smile_graph=smile_graph, clique_graph=clique_graph)
